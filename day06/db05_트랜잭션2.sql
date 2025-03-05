413f1b059d340729e476cdb19824ada275fe6fae
phm0423 <qkrgusalsok@gmail.com>
2025-03-05 17:28:50 +0900

    6일차 커밋

---
+++ day06/db05_트랜잭션2.sql
@@ -0,0 +1,37 @@
+-- 트랜잭션
+START TRANSACTION;
+INSERT INTO Book VAlUES(99, '데이터베이스', '한빛', 25000);
+
+SELECT bookname AS 'bookname1' FROM Book WHERE bookid = 99;
+-- 저장포인트
+SAVEPOINT a;
+
+UPDATE Book SET bookname = '데이터베이스 개론' WHERE bookid = 99;
+ 
+SELECT bookname AS 'bookname2' FROM Book WHERE bookid = 99;
+SAVEPOINT b;
+
+UPDATE Book SET bookname = '데이터베이스 개론 및 실습' WHERE bookid = 99;
+ 
+SELECT bookname AS 'bookname3' FROM Book WHERE bookid = 99;
+ROLLBACK TO b;
+
+SELECT bookname 'bookname4' FROM Book WHERE bookid = 99;
+ROLLBACK TO a;
+
+SELECT bookname 'bookname5' FROM Book WHERE bookid = 99;
+COMMIT;
+
+-- START TRANSACTION에서 COMMIT 까지는 트랜잭션이 살아있음
+-- SAVEPOINT는 트랜잭션 상에서 ROLLBACK TO로 이동가능
+-- ROLLBACK은 전부 취소
+START TRANSACTION;
+UPDATE Book SET bookname = '데이터베이스 개론 및 실습2' WHERE bookid = 99;
+
+SELECT bookname 'bookname6' FROM Book WHERE bookid = 99;
+ROLLBACK;
+
+SELECT bookname 'bookname7' FROM Book WHERE bookid = 99;
+
+DELETE FROM Book WHERE bookid = 99;
+COMMIT;
